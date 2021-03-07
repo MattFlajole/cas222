bd5462ce3af231941e665a9291727dad08d33bee
Matt Flajole <matthew.flajole@pcc.edu>
2021-03-07 15:48:37 -0800

    completed forms

--- template/includes/header.inc.html.php
+++
@@ -1,6 +0,0 @@
-<?php define('BASE_URL', '/cas222'); ?>
-
-<header>
-    <a href="<?php echo BASE_URL ?>/home"><img src="<?php echo BASE_URL ?>/template/images/logo.png" alt="Ace in the Hole Logo"></a>
-    <h1>Ace in the hole Multisport Events</h1>
-</header>
--- template/template.php
+++ template/template.php
@@ -12,52 +12,57 @@ Programmer: Matthew Flajole
         <title>Ace in the Hole Multisport Events</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="styles/reset.css" rel="stylesheet" type="text/css">
-        <link href="styles/helper.css" rel="stylesheet" type="text/css">
-        <link href="styles/grid.css" rel="stylesheet" type="text/css">
-        <link href="styles/style.css" rel="stylesheet" type="text/css">
+        <link href="../styles/reset.css" rel="stylesheet" type="text/css">
+        <link href="../styles/helper.css" rel="stylesheet" type="text/css">
+        <link href="../styles/grid.css" rel="stylesheet" type="text/css">
+        <link href="../styles/style.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Lemonada:wght@300;700&display=swap" rel="stylesheet">
     </head>
     <body>
-        <?php include 'includes/header.inc.html.php'; ?>
+        <div id="wrapper">
 
-        <?php include 'includes/navigation.inc.html.php'; ?>
-    <main>
-            <section class="top">
-                <div class="photo">
-                    <p>image goes here <br><br><br><br><br><br><br><br><br></p>
-                </div>
-                <h2>Heading 2</h2>
-                <p>Jump five feet high and sideways when a shadow moves human give me attention meow but rub butt on table so love blinks and purr purr purr purr yawn so poop on floor and watch human clean up or the door is opening! how exciting oh, it's you, meh or poop on couch. Cough furball. More napping, more napping all the napping is exhausting sleep on my human's head plop down in the middle where everybody walks put toy mouse in food bowl run out of litter box at full speed at four in the morning wake up owner meeeeeeooww scratch at legs and beg for food then cry and yowl until they wake up at two pm jump on window and sleep while observing the bootyful cat next door that u really like but who already has a boyfriend end up making babies with her and let her move in and meowwww yet plan your travel. Inspect anything brought into the house knock dish off table head butt cant eat out of my own dish for lick yarn hanging out of own butt yet leave fur on owners clothes but eat and than sleep on your face gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye if it smells like fish eat as much as you wish. Destroy couch as revenge plan your travel and commence midnight zoomies for making sure that fluff gets into the owner's eyes caticus cuteicus.</p>
-            </section>
-            <aside class="sidebar">
-                <h3>Heading 3</h3>
-                <ul>
-                    <li>List Item 1</li>
-                    <li>List Item 2</li>
-                    <li>List Item 3</li>
-                    <li>List Item 4</li>
-                    <li>List Item 5</li>
-                    <li>List Item 6</li>
-                    <li>List Item 7</li>
-                    <li>List Item 8</li>
-                    <li>List Item 9</li>
-                    <li>List Item 10</li>
-                    <li>List Item 11</li>
-                    <li>List Item 12</li>
-                    <li>List Item 13</li>
-                    <li>List Item 14</li>
-                    <li>List Item 15</li>
-                    <li>List Item 16</li>
-                    <li>List Item 17</li>
-                    <li>List Item 18</li>
-                    <li>List Item 19</li>
-                    <li>List Item 20</li>
-                </ul>
-            </aside>
-        </main>
+            <?php include '../includes/header.inc.html.php'; ?>
 
-        <?php include 'includes/footer.inc.html.php'; ?>
+            <?php include '../includes/navigation.inc.html.php'; ?>
+
+            <main>
+                <section class="top">
+                    <div class="photo">
+                        <p>image goes here <br><br><br><br><br><br><br><br><br></p>
+                    </div>
+                    <h2>Heading 2</h2>
+                    <p>Jump five feet high and sideways when a shadow moves human give me attention meow but rub butt on table so love blinks and purr purr purr purr yawn so poop on floor and watch human clean up or the door is opening! how exciting oh, it's you, meh or poop on couch. Cough furball. More napping, more napping all the napping is exhausting sleep on my human's head plop down in the middle where everybody walks put toy mouse in food bowl run out of litter box at full speed at four in the morning wake up owner meeeeeeooww scratch at legs and beg for food then cry and yowl until they wake up at two pm jump on window and sleep while observing the bootyful cat next door that u really like but who already has a boyfriend end up making babies with her and let her move in and meowwww yet plan your travel. Inspect anything brought into the house knock dish off table head butt cant eat out of my own dish for lick yarn hanging out of own butt yet leave fur on owners clothes but eat and than sleep on your face gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye if it smells like fish eat as much as you wish. Destroy couch as revenge plan your travel and commence midnight zoomies for making sure that fluff gets into the owner's eyes caticus cuteicus.</p>
+                </section>
+                <aside class="sidebar">
+                    <h3>Heading 3</h3>
+                    <ul>
+                        <li>List Item 1</li>
+                        <li>List Item 2</li>
+                        <li>List Item 3</li>
+                        <li>List Item 4</li>
+                        <li>List Item 5</li>
+                        <li>List Item 6</li>
+                        <li>List Item 7</li>
+                        <li>List Item 8</li>
+                        <li>List Item 9</li>
+                        <li>List Item 10</li>
+                        <li>List Item 11</li>
+                        <li>List Item 12</li>
+                        <li>List Item 13</li>
+                        <li>List Item 14</li>
+                        <li>List Item 15</li>
+                        <li>List Item 16</li>
+                        <li>List Item 17</li>
+                        <li>List Item 18</li>
+                        <li>List Item 19</li>
+                        <li>List Item 20</li>
+                    </ul>
+                </aside>
+            </main>
+        </div>
+    </body>
+
+        <?php include '../includes/footer.inc.html.php'; ?>
 
 </html>

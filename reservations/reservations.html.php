a84ee92c3e48d5d29c8e8c99f167f14368baeb8b
Matt Flajole <matthew.flajole@pcc.edu>
2021-03-07 20:36:41 -0800

    fixed css bug

--- reservations/reservations.html.php
+++ reservations/reservations.html.php
@@ -46,7 +46,7 @@ Programmer: Matthew Flajole
                             <li>Commemorative Finisher Medal</li>
                             <li>Accurate Chip  Timing for competitive races</li>
                             <li>Ace in the Hole Multisport Weekend Tech Shirt</li>
-                            <li>Post-event Party & entertainment</li>6
+                            <li>Post-event Party & entertainment</li>
                         </ul>
                     </div>
                 </aside>
@@ -55,31 +55,31 @@ Programmer: Matthew Flajole
                         <form method="post" action=" ">
                             <div class="hint">Required fields are marked with an asterisk (*).</div>
                             <input name="honeypot" type="text" value="" id="honeypot" class="honeypot" />
-                            <label for="myname">* Name:</label>
+                            <label for="myname">* Name</label>
                             <input type="text" name="myname" id="myname" required>
-                            <label for="myage">* Age:</label>
+                            <label for="myage">* Age</label>
                             <input type="number" name="myage" id="myage" min="1" max="80" required>
-                            <label for="myrole">* Role:</label>
+                            <label for="myrole">* Role</label>
                             <select size="1" name="myrole" id="myrole">
                                 <option>Choose role</option>
                                 <option value="athlete">Athlete</option>
                                 <option value="volunteer">Volunteer</option>
                             </select>
-                            <label for="myphone">* Phone Number:</label>
+                            <label for="myphone">* Phone Number</label>
                             <input type="tel" name="myphone" id="myphone">
                             <label for="myemail">* E-mail Address:</label>
                             <input type="email" name="myemail" id="myemail" required">
-                            <label for="myemergname">* Emergency Contact Name:</label>
+                            <label for="myemergname">* Emergency Contact Name</label>
                             <input type="text" name="myemergname" id="myemergname" required>
-                            <label for="myemergphone">* Emergency Contact Phone Number:</label>
+                            <label for="myemergphone">* Emergency Contact Phone</label>
                             <input type="tel" name="myemergphone" id="myemergphone" required>
-                            <label for="mygender">Gender Identification:</label>
+                            <label for="mygender">Gender Identification</label>
                             <select size="1" name="mygender" id="mygender">
                                 <option>Select Gender</option>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                             </select>
-                            <label for="myshirtsize">T-shirt Size:</label>
+                            <label for="myshirtsize">T-shirt Size</label>
                             <select size="1" name="myshirtsize" id="myshirtsize">
                                 <option>Select T-shirt Size</option>
                                 <option value="small">Small</option>
@@ -87,7 +87,7 @@ Programmer: Matthew Flajole
                                 <option value="large">Large</option>
                                 <option value="x-large">X-Large</option>
                             </select>
-                            <label for="mysatevent">* Saturday Events Registering For:</label>
+                            <label for="mysatevent">Saturday Events</label>
                             <select size="1" name="mysatevent" id="mysatevent">
                                 <option>Select Event</option>
                                 <option value="long course triathlon">Long Course Triathlon</option>
@@ -95,7 +95,7 @@ Programmer: Matthew Flajole
                                 <option value="10k">10 K Race</option>
                                 <option value="half marathon">Half Marathon</option>
                             </select>
-                            <label for="mysunevent">* Sunday Events Registering For:</label>
+                            <label for="mysunevent">Sunday Events</label>
                             <select size="1" name="mysunevent" id="mysunevent">
                                 <option>Select Event</option>
                                 <option value="sprint triathlon">Sprint Triathlon</option>
